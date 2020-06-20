f472270f3d945b52bab13be94b830319124ba6bc
Todd Riemenschneider <twr14152@gmail.com>
2020-06-20 18:38:46 -0400

    fizzbuzz-thingy

--- ch3_4_control_flow.rs
+++ ch3_4_control_flow.rs
@@ -1,13 +1,26 @@
 fn main() {
     // Fizz-buzz-like-thingy
-    for num in 1..100 {
+    for num in 1..20 {
         if num % 3 == 0 && num % 5 == 0 {
             println!("fizz buzz");
-        } else if num % 3 == 0 || num % 5 == 0 {
+        } else if num % 3 == 0 {
             println!("fizz");
+        } else if num % 5 == 0 {
+            println!("buzz")
         } else  {
             println!("{}", num);
         }
     }
-
+    println!("****************************");
+    for num in (1..20).rev() {
+        if num % 2 == 0 && num % 5 == 0 {
+            println!("{} divisible by 2 and 5", num);
+        } else if num % 2 == 0 {
+            println!("{} divisible by 2", num);
+        } else if num % 5 == 0 {
+            println!("{} divisible by 5", num);
+        } else {
+            println!("{}", num);
+        }
+}
 }
