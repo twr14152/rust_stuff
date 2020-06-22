7d36aa3dd2eec6ee4f234125224baf86334bcb63
Todd Riemenschneider <twr14152@gmail.com>
2020-06-22 09:36:44 -0400

    update

--- ch4_ref_borrowing.rs
+++ ch4_ref_borrowing.rs
@@ -8,13 +8,13 @@ fn main() {
     // goes out of scope
     let len = calculate_length(&s1); //&s1 creates reference to val at s1
 
-    println!("The length of '{}' is {}.", s1, len);
-
-    println!("Try and have borrower modify a value");
+    println!("The length of '{}' is {}.\n", s1, len);
+    println!("\n");
+    println!("Have borrower modify a value.....\n");
     let mut s2 = String::from("Inital value");
-    println!("value of s2 to start: {} ", s2);
+    println!("Value of s2 to start:\n {} \n", s2);
     change(&mut s2);
-    println!("value of s2 after change(): {}", s2);
+    println!("Value of s2 after change():\n {}\n", s2);
 
 }
 fn calculate_length(s: &String) -> usize { // s is a ref to a string
