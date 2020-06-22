6cc672ce66fc5f1e46170b778177eecffdef85f7
Todd Riemenschneider <twr14152@gmail.com>
2020-06-22 18:58:13 -0400

    update

--- ch4_ref_borrowing.rs
+++ ch4_ref_borrowing.rs
@@ -1,4 +1,8 @@
 //Having reference to a value is called borrowing
+//
+//At any given time you can have either one mutable reference or multiple immutable references
+//References must always be valid
+//Quoted from "the book"
 
 fn main() {
     let s1 = String::from("complex string variable");
@@ -15,7 +19,29 @@ fn main() {
     println!("Value of s2 to start:\n {} \n", s2);
     change(&mut s2);
     println!("Value of s2 after change():\n {}\n", s2);
+    println!("\nMore fun will mutable vars and reference (borrowers)\n\n");
+    let mut s3 = String::from("Whats up?");
+    {
+    let r1 = &mut s3;
+    println!("{}", r1)
+    } //r1 goes out of scope - so new ref can be made
+    let r2 = &mut s3; 
+    println!("{}", r2);
 
+    println!("\nThis next section talks about immutable borrowers vs mutable\n\n");
+    println!("\nYou cannot have mutable references while you have immutable references\n");
+    let mut t1 = String::from("Hellllloooooo");
+
+    let r3 = &t1; 
+    let r4 = &t1;
+    let r5 = &t1;
+
+    println!("{}, {}, and {}\n", r3, r4, r5);
+    
+    let r6 = &mut t1;
+    //let r7 = &mut t1; //cannot borrow `t1` as mutable more than once at a time
+    //println!("{}, and {}\n", r6, r7); //cannot borrow `t1` as mutable more than once at a time
+    println!("{}\n", r6);
 }
 fn calculate_length(s: &String) -> usize { // s is a ref to a string
     s.len()
